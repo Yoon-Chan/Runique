7a18aa6f28fa833b13fef9ecc277903d234c5c66
윤찬 <vsvx13@naver.com>
2025-05-09 17:36:41 +0900

    [feat]: Library 모듈 관련 Plugin 구현

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,42 @@
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+import com.example.convention.ExtensionType
+import com.example.convention.configureBuildTypes
+import com.example.convention.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidLibraryConventionPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+//            val androidExtension = project.extensions.getByName("android")
+
+            extensions.configure<LibraryExtension>("android") {
+                configureKotlinAndroid(this)
+
+                configureBuildTypes(
+                    commonExtension = this,
+                    extensionType = ExtensionType.LIBRARY
+                )
+
+                defaultConfig {
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+            }
+
+            dependencies {
+                "testImplementation"(kotlin("test"))
+            }
+        }
+    }
+}
--- build-logic/convention/src/main/java/com/example/convention/BuildTypes.kt
+++ build-logic/convention/src/main/java/com/example/convention/BuildTypes.kt
@@ -3,6 +3,7 @@ package com.example.convention
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.BuildType
 import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import org.gradle.api.Project
 
@@ -32,7 +33,7 @@ internal fun Project.configureBuildTypes(
                 }
             }
             ExtensionType.LIBRARY -> {
-                extensions.configure<ApplicationExtension>("android") {
+                extensions.configure<LibraryExtension>("android") {
                     buildTypes {
                         debug {
                             configureDebugBuildType(apiKey)

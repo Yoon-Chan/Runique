8a54b31c401844cf100b9da5e86429ecc186a97b
윤찬 <vsvx13@naver.com>
2025-06-16 13:18:12 +0900

    [feat]: logout 구현

--- core/database/src/main/java/com/example/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/example/core/database/RunDatabase.kt
@@ -14,7 +14,7 @@ import com.example.core.database.entity.RunPendingSyncEntity
         RunPendingSyncEntity::class,
         DeletedRunSyncEntity::class
     ],
-    version = 1
+    version = 2
 )
 abstract class RunDatabase : RoomDatabase() {
 
--- core/domain/src/main/java/com/example/core/domain/run/RunRepository.kt
+++ core/domain/src/main/java/com/example/core/domain/run/RunRepository.kt
@@ -7,7 +7,9 @@ import kotlinx.coroutines.flow.Flow
 interface RunRepository {
     fun getRuns(): Flow<List<Run>>
     suspend fun fetchRuns(): EmptyResult<DataError>
-    suspend fun upsertRun(run: Run, mapPicture :ByteArray): EmptyResult<DataError>
+    suspend fun upsertRun(run: Run, mapPicture: ByteArray): EmptyResult<DataError>
     suspend fun deleteRun(id: RunId)
     suspend fun syncPendingRuns()
+    suspend fun logout(): EmptyResult<DataError.Network>
+    suspend fun deleteAllRuns()
 }
